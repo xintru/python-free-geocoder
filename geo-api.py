121cb1afca2309ca30dcc24f878c3f41b3705a49
xintru <xintru@gmail.com>
2020-09-13 01:23:40 +0700

    error handling

--- geo-api.py
+++ geo-api.py
@@ -1,17 +1,25 @@
-
-from flask import Flask, request, jsonify
+from flask_api import FlaskAPI, status
+from flask import request
 from geopy.geocoders import Nominatim
-import json
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 
 
 @app.route('/get_geocoder_data')
 def get_geocoder_data():
-    address = request.args.get('address', "")
-    geolocator = Nominatim(user_agent="agent")
-    location = geolocator.geocode(address, addressdetails=True)
-    address_data = location.raw.get('address')
-    display_name = location.raw.get('display_name')
+    try: 
+        address = request.args.get('address', "")
+        geolocator = Nominatim(user_agent="agent")
+        location = geolocator.geocode(address, addressdetails=True)
+        address_data = location.raw.get('address')
+        display_name = location.raw.get('display_name')
 
-    return jsonify(address_data=address_data, display_name=display_name, lat=location.latitude, lng=location.longitude)
+        return { 
+            'address_data': address_data,
+            'display_name': display_name,
+            'lat': location.latitude, 
+            'lng': location.longitude 
+            }
+    except : 
+        return { 'message': 'Invalid query, data not found.' }, status.HTTP_404_NOT_FOUND
+
